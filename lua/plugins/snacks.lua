185c4615a481498ef1096fc63f3e6cb61f62d062
kshitiz adhikari <kshitiz.adhikari@azrachit.com>
2026-07-02 10:28:46 +0545

    make the snacks explorer/sidebar on the right

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      -- Configure the picker windows
       win = {
         input = {
           keys = {
@@ -10,12 +11,17 @@ return {
           },
         },
       },
-    },
 
-    explorer = {
-      layout = {
-        layout = {
-          position = "right",
+      -- Configure the explorer source specifically
+      sources = {
+        explorer = {
+          layout = {
+            preset = "sidebar", -- Use a preset or define custom layout
+            layout = {
+              position = "right",
+              width = 30, -- Set width if you want
+            },
+          },
         },
       },
     },
